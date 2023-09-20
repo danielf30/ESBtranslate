daa460b6b9f050ed610c7bcc7594e01dc6c8a4c1
Daniel Fajardo <danielf.fajardop30@gmail.com>
2023-09-20 13:55:29 -0500

    first deploy

---
+++ MyFunction1/__init__.py
@@ -0,0 +1,19 @@
+import azure.functions as func
+import json
+import dicttoxml
+
+def main(myQueueItem: func.ServiceBusMessage, binder: func.Out[str], log: func.Logger):
+    # Logging
+    data = myQueueItem.get_body().decode('utf-8')
+    log.info(f"Mensaje de Service Bus recibido: {data}")
+
+    # Convertir JSON a XML
+    json_obj = json.loads(data)
+    xml_str = dicttoxml.dicttoxml(json_obj, custom_root="Root", ids=False).decode('utf-8')
+    
+    # Usar idCiudadano como el nombre del archivo
+    file_name = f"{json_obj.get('idCiudadano', 'default')}.xml"
+
+    # Usar binder para escribir en Blob Storage
+    output_blob: func.Out[str] = binder.bind(func.Out[str], blobPath=f"ciudadanosBlobContainer/{file_name}")
+    output_blob.set(xml_str)
